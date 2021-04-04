fb9e5a93b7ad2a128e6583ed946ef079825a17aa
Luiza Biasoto <lpbiasoto@gmail.com>
2021-04-04 20:52:59 -0300

    resolvido bug cross-origin

--- backend/controller.py
+++ backend/controller.py
@@ -1,14 +1,19 @@
 from flask import Flask, request, jsonify
 from flask_caching import Cache
+from flask_cors import CORS, cross_origin
 from business.pe_analysis import *
 
 config = {
     "DEBUG": True,
     "CACHE_TYPE": "SimpleCache",
-    "CACHE_DEFAULT_TIMEOUT": 300
+    "CACHE_DEFAULT_TIMEOUT": 300,
+    "SECRET_KEY":"pipoca de caramelo",
+    "CORS_HEADERS": 'Content-Type'
 }
 app = Flask(__name__)
 app.config.from_mapping(config)
+
+cors = CORS(app, resources={r"/gold_PE_grid": {"origins": "http://localhost:port"},r"/": {"origins": "http://localhost:port"}})
 cache = Cache(app)
 
 @app.route('/')
@@ -16,6 +21,7 @@ def main():
     return "API rodando"
 
 @app.route('/gold_PE_grid/')
+@cross_origin(origin='localhost',headers=['Content- Type','Authorization'])
 def gold_PE_grid():
     ativos_dict = {"Gold Futures": 8830, "GOLD": 13928, "Cobre Futuros": 8831}
     pe_grid = get_yearly_PE_grid(datetime.now().date()-timedelta(365), datetime.now().date(), ativos_dict)
